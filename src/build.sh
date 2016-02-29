b182c9f28069cbc06bef676ab8d17e401481d77a
chribben <chribben@gmail.com>
2016-02-29 22:05:44 +0100

    Add build script for linux/mac

---
+++ src/build.sh
@@ -0,0 +1,10 @@
+npm install
+cd ./node_modules/edge
+electron_version=npm ll -p --depth=0 electron-prebuilt | grep -o "@.*:" | sed 's/.$//; s/^.//'
+HOME=~/.electron-gyp node-gyp rebuild --target=$electron_version --arch=x64 --dist-url=https://atom.io/download/atom-shell
+cd ./ScriptEngine
+dnu restore 
+dnu build
+dnu publish
+cd ..
+find . -name "*.dll" -type f -exec cp {} . \;
