5cbe942a303e8edf268932eccce46185aea9e3ec
chribben <chribben@gmail.com>
2016-03-01 12:53:52 +0100

    Update build script

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,7 @@
 npm install
+npm install node-gyp -g
 cd ./node_modules/edge
-electron_version=npm ll -p --depth=0 electron-prebuilt | grep -o "@.*:" | sed 's/.$//; s/^.//'
+electron_version=$(npm ll -p --depth=0 electron-prebuilt | grep -o "@.*:" | sed 's/.$//; s/^.//')
 HOME=~/.electron-gyp node-gyp rebuild --target=$electron_version --arch=x64 --dist-url=https://atom.io/download/atom-shell
 cd ./ScriptEngine
 dnu restore 
