9a4c85640d3bf09dba68e5f9ade3fb183818097f
Christian Jacobsen <chribben@gmail.com>
2016-03-02 22:24:28 +0100

    Add script for installing pre-reqs in Ubuntu

---
+++ src/install_stuff.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install git unzip curl -y
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+sudo apt-get install -y nodejs
+curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
+sudo apt-get install libunwind8 gettext libssl-dev libcurl4-openssl-dev zlib1g libicu-dev uuid-dev -y
+source ~/.dnx/dnvm/dnvm upgrade -r coreclr
+
