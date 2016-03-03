cdeb1c7f0613c148e30f49af0fbc7a3bc477b2f0
christian jacobsen <chribben@gmail.com>
2016-03-03 23:10:23 +0100

    Update install script

--- src/install_stuff.sh
+++ src/install_stuff.sh
@@ -3,7 +3,8 @@ sudo apt-get update -y
 sudo apt-get install git unzip curl -y
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 sudo apt-get install -y nodejs
-curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
+sudo apt-get install build-essential -y
+curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh 
+source ~/.dnx/dnvm/dnvm.sh
 sudo apt-get install libunwind8 gettext libssl-dev libcurl4-openssl-dev zlib1g libicu-dev uuid-dev -y
-source ~/.dnx/dnvm/dnvm upgrade -r coreclr
-sudo apt-get install build-essential
+dnvm upgrade -r coreclr
