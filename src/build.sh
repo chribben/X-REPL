1ea20f587ba27ec7bd68bd26cdc9a3656bd25cbc
christian jacobsen <chribben@gmail.com>
2016-03-04 07:41:03 +0100

    sudo

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,5 @@
 npm install
-npm install node-gyp -g
+sudo npm install node-gyp -g
 cd ./node_modules/edge
 electron_version=$(npm ll -p --depth=0 electron-prebuilt | grep -o "@.*:" | sed 's/.$//; s/^.//')
 HOME=~/.electron-gyp node-gyp rebuild --target=$electron_version --arch=x64 --dist-url=https://atom.io/download/atom-shell
